adb7258952a226ff70690119911e5abf4556c381
BAmit1234 <49117018+BAmit1234@users.noreply.github.com>
2021-08-08 01:48:53 +0530

    Add files via upload

---
+++ GinProject/go.mod
@@ -0,0 +1,9 @@
+module github.com/rahmanfadhil/gin-bookstore
+
+go 1.14
+
+require (
+	github.com/gin-gonic/contrib v0.0.0-20201101042839-6a891bf89f19
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.12
+)
---
+++ GinProject/main.go
@@ -0,0 +1,28 @@
+package main
+
+package controllers
+package models
+import (
+	"controllers"
+	"models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	// Connect to database
+	models.ConnectDatabase()
+	r.LoadHTMLFiles("templates/index.html")
+
+	// Routes
+	r.GET("/books", controllers.FindBooks)
+	r.GET("/books/:id", controllers.FindBook)
+	r.POST("/books", controllers.CreateBook)
+	r.PATCH("/books/:id", controllers.UpdateBook)
+	r.DELETE("/books/:id", controllers.DeleteBook)
+
+	// Run the server
+	r.Run()
+}
